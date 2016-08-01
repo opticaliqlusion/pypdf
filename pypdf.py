8ceb5c998ee69e0528ff2f9a26f9e43a49b04b8c
opticaliqlusion <john.laky@gmail.com>
2016-07-31 20:31:53 -0700

    added some simple tree traversal, id packing, and printing the tree, wrapping nodes with objects

--- pypdf.py
+++ pypdf.py
@@ -1,26 +1,20 @@
 import os
 import sys
-import platform
 import copy
 from ply import lex
 from ply import yacc
 import pdb
+import StringIO
 
 #
 #   SCANNER
 #
 
 tokens = (
-  #'REFERENCE',
-   'PERCENT',
-   'HYPHEN',
    'BOOL',
    'NUMBER',
-   'SPACE',
    'LPAREN',
    'RPAREN',
-   'FSLASH',
-   'EOL',
    'ID',
    'HEX',
    'LTLT',
@@ -28,8 +22,6 @@ tokens = (
    'LBRACKET',
    'RBRACKET',
    'STREAM',
-   'BINARY',
-   'NULL',
    'COMMENT',
    'OBJ',
    'ENDOBJ',
@@ -47,7 +39,6 @@ class PdfScanner():
         t_HEX       = r'\<[0-9A-F]*\>'
         t_LPAREN    = r'\('
         t_RPAREN    = r'\)'
-        t_FSLASH    = r'/'
         t_LBRACKET  = r'\['
         t_RBRACKET  = r'\]'
         t_LTLT      = r'\<\<'
@@ -85,15 +76,11 @@ class PdfScanner():
     def pop_until(self, target_token):
         children = []
         while True:
-            try:
-                tok = self.token_stack.pop()
-                if tok['type'] == target_token:
-                    break
-                else:
-                    children.insert(0, tok)
-            except:
-                pdb.set_trace()
-                pass
+            tok = self.token_stack.pop()
+            if tok.type == target_token:
+                break
+            else:
+                children.insert(0, tok)
 
         return children
 
@@ -101,10 +88,10 @@ class PdfScanner():
         return token
 
     def handle_gtgt(self, token):
-        return { 'type' : 'dictionary', 'children' : self.pop_until('ltlt') }
+        return Node({ 'type' : 'dictionary', 'children' : self.pop_until('ltlt') })
 
     def handle_rbracket(self, token):
-        return { 'type' : 'array', 'children' : self.pop_until('lbracket') }
+        return Node({ 'type' : 'array', 'children' : self.pop_until('lbracket') })
 
     def handle_endobj(self, token):
 
@@ -113,14 +100,14 @@ class PdfScanner():
         n1 = self.token_stack.pop()
         n2 = self.token_stack.pop()
 
-        return {'type' : 'obj', 'children':children, 'coords':(n1,n2)}
+        return Node({'type' : 'obj', 'children':children, 'coords':(n1,n2)})
 
     def get_token(self):
 
         tok = self.token_stream.pop()
 
         try:
-            res = getattr(self,'handle_{0}'.format(tok['type']))(tok)
+            res = getattr(self,'handle_{0}'.format(tok.type))(tok)
         except AttributeError:
             res = self.handle_generic(tok)
 
@@ -141,7 +128,7 @@ class PdfScanner():
     def transform_tokens(self, tokens):
         transformed = []
         for token in tokens:
-            transformed.append({'type':token.type.lower(), 'value':token.value})
+            transformed.append(Node({'type':token.type.lower(), 'value':token.value}))
         return transformed
 
     def __init__(self, input):
@@ -161,11 +148,35 @@ class PdfScanner():
         self.token_stream = self.transform_tokens(self.token_stream)
         self.parse_token_stream()
 
-        self.tree = {'type':'pdf', 'children':copy.deepcopy(self.token_stack)}
+        self.tree = Node({'type':'pdf', 'children':copy.deepcopy(self.token_stack)})
 
         IDKeyValuePacker().visit(self.tree)
 
         return
+
+class Node():
+    def __init__(self, attributes):
+        for k,v in attributes.iteritems():
+            setattr(self, k, v)
+
+        # some defaults
+        if not hasattr(self, 'children'):
+            setattr(self, 'children', [])
+
+        if not hasattr(self, 'type'):
+            setattr(self, 'type', 'UNKNOWN')
+
+        if not hasattr(self, 'value'):
+            setattr(self, 'value', None)
+
+    def __str__(self):
+        if self.value:
+            return '<PDF_Node-{0} {1}>'.format(self.type, str(self.value)[:10])
+        else:
+            return '<PDF_Node-{0}>'.format(self.type)
+
+    def __repr__(self):
+        return self.__str__()
 
 #
 #   TREE TRAVERSAL, IN THE STYLE OF AST
@@ -174,14 +185,13 @@ class PdfScanner():
 
 class PdfTreeVisitor():
     def visit_generic(self, node):
-        if 'children' in node.keys():
-            for child in node['children']:
-                try:
-                    handler = getattr(self, 'visit_{0}'.format(child['type']))
-                except AttributeError:
-                    handler = self.visit_generic
+        for child in node.children:
+            try:
+                handler = getattr(self, 'visit_{0}'.format(child.type))
+            except AttributeError:
+                handler = self.visit_generic
 
-                handler(child)
+            handler(child)
 
     def visit(self, tree):
         return self.visit_generic(tree)
@@ -200,7 +210,7 @@ class StreamIterator(PdfTreeVisitor):
 class IDKeyValuePacker(PdfTreeVisitor):
     def visit_dictionary(self, node):
         new_children = []
-        old_children = copy.deepcopy(node['children'])
+        old_children = copy.deepcopy(node.children)
         old_children.reverse()
 
         while True:
@@ -208,20 +218,39 @@ class IDKeyValuePacker(PdfTreeVisitor):
                 tok = old_children.pop()
             except IndexError:
                 break
-            if tok['type'] == 'id':
+            if tok.type == 'id':
                 value = old_children.pop()
-                tok['children'] = [ value ]
+                tok.children = [ value ]
+
             new_children.append(tok)
 
-        node['children'] = new_children
+        node.children = new_children
         pass
+
+class PDFTreePrinter(PdfTreeVisitor):
+    def __init__(self, tree):
+        self.sio = StringIO.StringIO()
+        self.depth = 0
+
+        self.visit(tree)
+
+    def __str__(self):
+        return self.sio.getvalue()
+
+    def visit_generic(self, node):
+        self.sio.write('{0}{1}\n'.format('  '*self.depth, str(node)))
+        self.depth += 1
+        PdfTreeVisitor.visit_generic(self, node)
+        self.depth -= 1
 
 def main():
 
     test_data = open(sys.argv[1]).read()
     scanner = PdfScanner(test_data)
 
-    pdb.set_trace()
+    printable = PDFTreePrinter(scanner.tree)
+    print(printable)
+
     return
 
 if __name__ == "__main__":
