e1dbc0e4bddde27aa05d36fa7f852afc2d6b0ab0
opticaliqlusion <john.laky@gmail.com>
2016-07-31 23:04:27 -0700

    replacing example setup info

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
   description = 'A PDF inspector',
   author = 'Opticaliqlusion',
   author_email = 'john.laky1@gmail.com',
-  url = 'https://github.com/peterldowns/mypackage',
-  download_url = 'https://github.com/opticaliqlusion/pypdf/archive',
+  url = 'https://github.com/opticaliqlusion/pypdf',
+  download_url = 'https://github.com/opticaliqlusion/pypdf/tarball/0.1',
   keywords = ['pdf', 'scanner'],
   classifiers = [],
 )
